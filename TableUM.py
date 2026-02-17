5300c65ef7ff57474602cb63bb71162e254daec2
SerVP <s_pahomov@mail.ru>
2026-02-18 00:14:38 +0700

    рабочая версия

--- TableUM.py
+++ TableUM.py
@@ -2,6 +2,8 @@ import time
 from tkinter import Tk, LabelFrame, Label, Button, CENTER, BOTH, N, S, IntVar, messagebox, ttk
 import random
 
+MAX_TIME = 10
+
 def finish(app):
     app.after_cancel(app.id_after)
     for i in app.winfo_children():
@@ -18,19 +20,15 @@ def finish(app):
             i.configure(bg="red")
         label_up.configure(text=f"Ошибок = {app.error_count}", font=("Arial", 60, "bold"))
         label_down.configure(text="Время вышло", font=("Arial", 60, "bold"))
-    if len(app.list_counts) == 0 and app.run:
+    if len(app.list_counts) == 0: #and app.run:
         label_up.configure(text=f"Ошибок = {app.error_count}", font=("Arial", 60, "bold"))
         label_down.configure(text="Проверка завершена", font=("Arial", 60, "bold"))
         if app.error_count != 0:
-            col = "orrange"
+            col = "#d35400"
         else:
             col = "green"
         for i in list_labels:
             i.configure(bg=col)
-# ??????????????????????????????????????
-    app.run = False
-
-
 
 def update(app):
     if app.run:
@@ -39,100 +37,73 @@ def update(app):
         if app.sec_count!=0:
             app.after(1000, update, app)
         else:
+            app.run = False
             finish(app)
 
-
 def button_name(app):
-
     app.stop_draw = False
     rand_but = random.randint(0, len(app.but_list)-1)  #  0..9
     des_val = app.list_of_num[app.index_task][0]*app.list_of_num[app.index_task][1]
     list_val = [des_val]
-
     for i in range(0,len(app.but_list)): # i in 0..9
         if i != rand_but:
             val = random.randint(1, 10) * random.randint(1, 10)
             while val in list_val:
                 val = random.randint(1, 10) * random.randint(1, 10)
             app.but_list[i].configure(text=val,
-                                      command=lambda right_btn_num=rand_but, er_btn=app.but_list[i]: next_with_error(app, right_btn_num, er_btn))
+                                      command=lambda right_btn_num=rand_but, er_btn=app.but_list[i]: next_with_error(app, right_btn_num, er_btn),
+                                      bg=app.btn_std_col
+                                      )
         else:
             val = des_val
-            app.but_list[i].configure(text=val, command=lambda: next_ex(app))
+            app.but_list[i].configure(text=val, command=lambda: next_ex(app), bg=app.btn_std_col)
         list_val.append(val)
 
-
 def next_with_error(app, right_btn_num, er_btn):
-
-    def come_back_colors():
-        right_btn.configure(bg=save)
-        er_btn.configure(bg=save)
-
     if not app.stop_draw:
         app.stop_draw = True
         right_btn = app.but_list[right_btn_num]
-        save = right_btn["bg"]
         right_btn.configure(bg="green")
         er_btn.configure(bg="red")
-        # right_btn.after(1000, come_back_colors)
-
+        app.error_count += 1
 
 def next_ex(app):
-    pass
-
-# def check(val, app):
-#     rez = app.list_of_num[app.index_task][0] * app.list_of_num[app.index_task][1]
-#     # print("*", val, rez, val==rez)
-#
-#     if len(app.list_counts) == 0 and app.run:
-#         finish(app)
-#
-#     if val==rez and app.run:
-#         app.sec_count = app.max_time
-#         app.time_label.configure(text=f"{app.sec_count} сек")
-#         app.index_task = random.choice(app.list_counts)
-#         try:
-#             app.list_counts.remove(app.index_task)
-#         except ValueError:
-#             print(app.list_counts)
-#             print(app.index_task)
-#         # print(app.list_of_num[app.index_task])
-#         try:
-#             app.ex_label.configure(text = f"{app.list_of_num[app.index_task][0]} X {app.list_of_num[app.index_task][1]}")
-#         except IndexError:
-#             print(app.list_of_num)
-#             print(app.index_task)
-#         button_name(app)
-#         app.count_task.set(app.count_task.get()+1)
-#
-#
-#     elif app.run:
-#         app.error_count+=1
-#         print("YOU WRONG!!! Error count =", app.error_count)
-
-
+    if len(app.list_counts) != 0:
+        app.sec_count = app.max_time
+        app.time_label.configure(text=f"{app.sec_count} сек")
+        app.index_task = random.choice(app.list_counts)
+        try:
+            app.list_counts.remove(app.index_task)
+        except ValueError:
+            print(app.list_counts)
+            print(app.index_task)
+        try:
+            app.ex_label.configure(text = f"{app.list_of_num[app.index_task][0]} X {app.list_of_num[app.index_task][1]}")
+        except IndexError:
+            print(app.list_of_num)
+            print(app.index_task)
+        button_name(app)
+        app.count_task.set(app.count_task.get()+1)
+    else:
+        app.run = False
+        finish(app)
 
 def init_APP(list_of_num, list_counts):
-
-
-
-
     app = Tk()
     app.title("TableUM")
     app.geometry("940x405")
     app.len_list_of_num = len(list_of_num)
     app.count_task = IntVar()
-    app.count_task.set(1)
+    app.count_task.set(0)
     app.list_of_num = list_of_num
     app.list_counts = list_counts
-    app.max_time = 10
+    app.max_time = MAX_TIME
     app.sec_count = app.max_time
     app.error_count = 0
     app.run = True
 
     app.index_task = random.choice(app.list_counts)
     app.list_counts.pop(app.index_task)
-    # print(app.list_of_num[app.index_task])
 
     ex_label_fr = LabelFrame(app, background="yellow")
     ex_label_fr.grid(row=0, column=0,  padx=10, pady=10)
@@ -140,7 +111,11 @@ def init_APP(list_of_num, list_counts):
     app.but_fr = LabelFrame(app)
     app.but_fr.grid(row=0, column=1)
 
-    app.progr_bar = ttk.Progressbar(app, length=900, variable=app.count_task)
+    app.progr_bar = ttk.Progressbar(app,
+                                    length=900,
+                                    variable=app.count_task,
+                                    maximum=len(app.list_counts)+1
+                                    )
     app.progr_bar.grid(row=1, column=0, columnspan=2, )
 
     but1 = Button(app.but_fr, font=("Arial", 25, "bold"), width=4)
@@ -155,6 +130,7 @@ def init_APP(list_of_num, list_counts):
     but10 = Button(app.but_fr, font=("Arial", 25, "bold"), width=4)
 
     app.but_list = [but1, but2, but3, but4, but5, but6, but7, but8, but9, but10]
+    app.btn_std_col = but1["bg"]
 
     button_name(app)
 
@@ -196,8 +172,7 @@ def init_APP(list_of_num, list_counts):
 
 
 def run():
-    # list_of_num = [[i, j] for i in range(1,11) for j in range(1,11)]
-    list_of_num = [[i, j] for i in range(1, 3) for j in range(1, 3)]
+    list_of_num = [[i, j] for i in range(1,11) for j in range(1,11)]
     list_counts = [i for i in range(0,len(list_of_num))]
 
     # print(list_counts)
